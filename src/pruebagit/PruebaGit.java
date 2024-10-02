74344da93fea85ccb7486b1d1b062251267feb97
CamilaAlfaro <camilaalfa20@gmail.com>
2024-10-02 14:10:51 -0600

    Primer commit

---
+++ src/pruebagit/PruebaGit.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebagit;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PruebaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Esto es la prueba de subir un proyecto a GitHub por medio de consola.");
+        System.out.print("\nCamila Alfaro - Int. Programacion");
+    }
+    
+}
